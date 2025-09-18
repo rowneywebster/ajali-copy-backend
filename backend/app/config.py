1ebfd5d33882ef9723bb2f436b0f9b4c3fc96331
rowneywebster <oumarowney@gmail.com>
2025-09-18 11:22:27 +0300

    production ready

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,18 +1,27 @@
-# app/config.py
+# # app/config.py
+# import os
+
+# class Config:
+#     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "superjwtsecret")
+    
+#     # Email
+#     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+#     MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
+#     MAIL_USE_TLS = True
+#     MAIL_USE_SSL = False
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+#     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+
+
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "superjwtsecret")
-    
-    # Email
-    MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+
 
--- backend/venv/bin/activate.csh
+++ backend/venv/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/rouma/moringa-school/SDF-PT10-Group-09/backend/venv
+setenv VIRTUAL_ENV /home/rouma/moringa-school/ajali-backend/backend/venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
