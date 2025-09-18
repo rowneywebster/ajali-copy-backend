88311d68fd69259750d5b25771413dba0a172db4
rowneywebster <oumarowney@gmail.com>
2025-09-18 11:33:27 +0300

    updated app

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,20 @@
-# run.py
-from app import create_app
+# # run.py
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from app import create_app, db
+from flask_migrate import upgrade
 
 app = create_app()
 
+# Run migrations at startup
+with app.app_context():
+    upgrade()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
